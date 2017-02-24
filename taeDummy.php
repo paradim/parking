e1734ed34ac6ffb202984454e503ed9dd2cc7157
INFORMATION Technology2 <INFORMATION Technology2>
2017-02-24 16:00:24 +0700

    include changes

--- taeDummy.php
+++ taeDummy.php
@@ -1,10 +1,10 @@
 <?php 
 include 'Changes.php';
 
-echo "1";
-// $ch = new Changes(100, 70);
+echo "1\n";
+$ch = new Changes(100, 70);
 
-// echo $ch.toString();
+echo $ch.toString();
 
-echo "2";
+echo "2\n";
 ?>
