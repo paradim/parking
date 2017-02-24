3828b105316308dfd22b1b25a890c8957f67def0
ccldmc <ann_cnp@hotmail.com>
2017-02-24 16:26:53 +0700

    ann 16.26

---
+++ getTransaction.php
@@ -0,0 +1,16 @@
+<?php
+//$LicenseID = $_POST["lic"];
+
+$servername = "localhost";
+$username = "root";
+$password = "xitgmLwnp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error);
+}
+echo "Connected successfully";
+?>
