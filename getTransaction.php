128b653dd06a9b530da9d759963e6586397eb911
ccldmc <ann_cnp@hotmail.com>
2017-02-24 16:50:34 +0700

    ann16.50

--- getTransaction.php
+++ getTransaction.php
@@ -4,13 +4,26 @@
 $servername = "localhost";
 $username = "root";
 $password = "xitgmLwmp";
+$dbname = "Parking";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn -> connect_error) {
-    die("Connection failed: " . $conn -> connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM transaction_tbl";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "found data";
+    }
+} else {
+    echo "0 results";
 }
-echo "Connected successfully";
+$conn->close();
 ?>
